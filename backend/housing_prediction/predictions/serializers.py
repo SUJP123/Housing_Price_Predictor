4a0d0612250f0867ed43cbf1faca22dc5e098bc3
SUJP123 <sujaypatel04@gmail.com>
2024-07-02 14:46:50 -0400

    config

--- backend/housing_prediction/predictions/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import RegionData
-
-class RegionDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RegionData
-        fields = '__all__'
--- backend/housing_prediction/predictions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import predict
-
-urlpatterns = [
-    path('predict/', predict, name='predict'),
-    path('predict/', predict),
-]
